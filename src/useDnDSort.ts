3508e9cd82dc48dff8f697ca4ebefd984eb397cd
Koichi Miyamoto <m5813k@gmail.com>
2021-06-01 03:44:45 +0900

    tmp

--- src/useDnDSort.ts
+++ src/useDnDSort.ts
@@ -16,7 +16,10 @@ interface DnDItem<T> {
   element: HTMLElement;
   hitArea: DOMRect;
 }
-
+interface MouseDownResolvedValue<T> {
+  item: DnDItem<T>;
+  event: React.MouseEvent<HTMLElement>;
+}
 // 返り値の型
 interface DnDSortResult<T> {
   key: string;
@@ -25,6 +28,17 @@ interface DnDSortResult<T> {
     ref: (element: HTMLElement | null) => void;
     onMouseDown: (event: React.MouseEvent<HTMLElement>) => void;
   };
+}
+
+function useRefArr<T>() {
+  const ref = useRef<DnDItem<T>[]>([]);
+  return [
+    ref.current as readonly DnDItem<T>[],
+    (newArr: DnDItem<T>[]) => {
+      ref.current.length = 0;
+      Array.prototype.push.apply(ref.current, newArr);
+    },
+  ] as const;
 }
 
 export const useDnDSort = <T>(defaultItems: T[]): DnDSortResult<T>[] => {
@@ -38,10 +52,10 @@ export const useDnDSort = <T>(defaultItems: T[]): DnDSortResult<T>[] => {
     return keyMap;
   }, []);
 
-  const dndItems = useRef<DnDItem<T>[]>([]).current;
+  const [dndItems, setDnDItems] = useRefArr<T>();
 
   const [resolveMouseDown, getMouseDownPromise] =
-    usePromise<{ item: DnDItem<T>; event: React.MouseEvent<HTMLElement> }>();
+    usePromise<MouseDownResolvedValue<T>>();
   const [resolveMouseUp, getMouseUpPromise] = usePromise<void>();
 
   async function listenDragUntilMouseUp(onMouseMove: (e: MouseEvent) => void) {
@@ -54,106 +68,13 @@ export const useDnDSort = <T>(defaultItems: T[]): DnDSortResult<T>[] => {
 
   const run = async () => {
     while (true) {
-      const {
-        item: dragItem,
-        event: { clientX, clientY },
-      } = await getMouseDownPromise();
-
-      // when sorting, this will be updated.
-      let downPos = { x: clientX, y: clientY };
-      const dragItemStyle = dragItem.element.style;
-
-      const { cursor, transform, zIndex } = dragItemStyle;
-      const prevStyle = { cursor, transform, zIndex };
-
-      // ドラッグしている要素のスタイルを上書き
-      Object.assign(dragItemStyle, {
-        transition: "",
-        cursor: "grabbing",
+      await lifecycle({
+        getMouseDownPromise,
+        listenDragUntilMouseUp,
+        dndItems,
+        setItems: setDnDItems,
+        render: (items) => setItems(items.map((v) => v.value)),
       });
-
-      // ドラッグ中の処理
-      const onMouseMove = (event: MouseEvent) => {
-        const mousePos = { x: event.clientX, y: event.clientY };
-
-        // assign drag item style
-        // マウスポインターの移動量を計算
-        const offset = sub(mousePos, downPos);
-
-        // ドラッグ要素の座標とスタイルを更新
-        Object.assign(dragItemStyle, {
-          zIndex: "100",
-          cursor: "grabbing",
-          transform: `translate(${offset.x}px,${offset.y}px)`,
-        });
-
-        // 入れ替え判定
-        // ホバーされている要素の配列の位置を取得
-        const hoveredIndex = dndItems.findIndex(
-          ({ hitArea, key }) =>
-            key !== dragItem.key && isHover(mousePos, hitArea)
-        );
-
-        // ホバーされている要素があれば、ドラッグしている要素と入れ替える
-        if (hoveredIndex >= 0) {
-          const dragIndex = dndItems.findIndex(
-            ({ key }) => key === dragItem.key
-          );
-          //sort
-          dndItems.splice(dragIndex, 1);
-          dndItems.splice(hoveredIndex, 0, dragItem);
-
-          // get previous position
-          const beforeRenderData = dndItems.map((item) => {
-            return {
-              ...item,
-              prevPos: getElementPosition(item.element),
-            };
-          });
-
-          // re-render
-          setItems(dndItems.map((v) => v.value));
-
-          // calculate new position and hitArea
-          const newItems = beforeRenderData.map((item) => {
-            item.element.style.transform = "";
-            item.element.style.transition = "";
-            const position = getElementPosition(item.element);
-            const hitArea = item.element.getBoundingClientRect();
-            return Object.assign({}, item, { position, hitArea });
-          });
-
-          // apply transform and animation
-          newItems
-            .map((item) => {
-              return {
-                diff: sub(item.prevPos, item.position),
-                style: item.element.style,
-                isDragItem: item.key == dragItem.key,
-              };
-            })
-            .forEach(({ diff, style, isDragItem }) => {
-              style.transform = `translate(${diff.x}px,${diff.y}px)`;
-
-              if (isDragItem) {
-                // update down position to ajust dragging offset
-                downPos = sub(mousePos, diff);
-              } else {
-                requestAnimationFrame(() => {
-                  style.transform = "";
-                  style.transition = "all 200ms";
-                });
-              }
-            });
-
-          dndItems.length = 0;
-          Array.prototype.push.apply(dndItems, newItems);
-        }
-      };
-
-      await listenDragUntilMouseUp(onMouseMove);
-      // ドラッグしてる要素に適用していたCSSを削除
-      Object.assign(dragItemStyle, { ...prevStyle });
     }
   };
   useEffect(() => {
@@ -170,12 +91,11 @@ export const useDnDSort = <T>(defaultItems: T[]): DnDSortResult<T>[] => {
       events: {
         ref: (element: HTMLElement | null) => {
           if (!element) return;
-
           const exists = dndItems.find((item) => item.key === key);
           if (exists === undefined) {
             element.style.transform = "";
             const hitArea = element.getBoundingClientRect();
-            return dndItems.push({ key, value, element, hitArea });
+            setDnDItems([...dndItems, { key, value, element, hitArea }]);
           }
         },
 
@@ -188,3 +108,117 @@ export const useDnDSort = <T>(defaultItems: T[]): DnDSortResult<T>[] => {
     };
   });
 };
+
+interface LifecycleProp<T> {
+  getMouseDownPromise: () => Promise<MouseDownResolvedValue<T>>;
+  listenDragUntilMouseUp: (
+    onMouseMove: (e: MouseEvent) => void
+  ) => Promise<void>;
+  dndItems: readonly DnDItem<T>[];
+  setItems: (items: DnDItem<T>[]) => void;
+  render: (items: DnDItem<T>[]) => void;
+}
+async function lifecycle<T>({
+  getMouseDownPromise,
+  listenDragUntilMouseUp,
+  dndItems,
+  setItems,
+  render,
+}: LifecycleProp<T>) {
+  const {
+    item: dragItem,
+    event: { clientX, clientY },
+  } = await getMouseDownPromise();
+
+  // when sorting, this will be updated.
+  let downPos = { x: clientX, y: clientY };
+  const dragItemStyle = dragItem.element.style;
+
+  const { cursor, transform, zIndex } = dragItemStyle;
+  const prevStyle = { cursor, transform, zIndex };
+
+  // ドラッグしている要素のスタイルを上書き
+  Object.assign(dragItemStyle, {
+    transition: "",
+    cursor: "grabbing",
+  });
+
+  // ドラッグ中の処理
+  const onMouseMove = (event: MouseEvent) => {
+    const mousePos = { x: event.clientX, y: event.clientY };
+
+    // assign drag item style
+    // マウスポインターの移動量を計算
+    const offset = sub(mousePos, downPos);
+
+    // ドラッグ要素の座標とスタイルを更新
+    Object.assign(dragItemStyle, {
+      zIndex: "100",
+      cursor: "grabbing",
+      transform: `translate(${offset.x}px,${offset.y}px)`,
+    });
+
+    // 入れ替え判定
+    // ホバーされている要素の配列の位置を取得
+    const hoveredIndex = dndItems.findIndex(
+      ({ hitArea, key }) => key !== dragItem.key && isHover(mousePos, hitArea)
+    );
+
+    // ホバーされている要素があれば、ドラッグしている要素と入れ替える
+    if (hoveredIndex === -1) return;
+    const dragIndex = dndItems.findIndex(({ key }) => key === dragItem.key);
+    //sort
+    const sorted = [...dndItems];
+    sorted.splice(dragIndex, 1);
+    sorted.splice(hoveredIndex, 0, dragItem);
+
+    // get previous position
+    const beforeRenderData = sorted.map((item) => {
+      return {
+        ...item,
+        prevPos: getElementPosition(item.element),
+      };
+    });
+
+    // re-render
+    render(sorted);
+
+    // calculate new position and hitArea
+    const newItems = beforeRenderData.map((item) => {
+      item.element.style.transform = "";
+      item.element.style.transition = "";
+      const position = getElementPosition(item.element);
+      const hitArea = item.element.getBoundingClientRect();
+      return Object.assign({}, item, { position, hitArea });
+    });
+
+    // apply transform and animation
+    newItems
+      .map((item) => {
+        return {
+          diff: sub(item.prevPos, item.position),
+          style: item.element.style,
+          isDragItem: item.key == dragItem.key,
+        };
+      })
+      .forEach(({ diff, style, isDragItem }) => {
+        style.transform = `translate(${diff.x}px,${diff.y}px)`;
+
+        if (isDragItem) {
+          // update down position to ajust dragging offset
+          downPos = sub(mousePos, diff);
+        } else {
+          requestAnimationFrame(() => {
+            style.transform = "";
+            style.transition = "all 200ms";
+          });
+        }
+      });
+
+    setItems(newItems);
+  };
+
+  await listenDragUntilMouseUp(onMouseMove);
+  // ドラッグしてる要素に適用していたCSSを削除
+  Object.assign(dragItemStyle, { ...prevStyle });
+}
